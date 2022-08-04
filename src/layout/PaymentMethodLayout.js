06073b367f8ac135ad1f524c435d5969a0a891d1
Sudharsun <psu.fabulo@gmail.com>
2022-08-05 01:41:46 +0530

    delivery cost

--- src/layout/PaymentMethodLayout.js
+++ src/layout/PaymentMethodLayout.js
@@ -8,6 +8,12 @@ const paymentMethodLogos = [
 	"ideal",
 ];
 const deliveryMethodLogos = ["inpost", "dpd", "dhl", "fedx"];
+const deliveryMethodCost = {
+	inpost: "20.00",
+	dpd: "12.00",
+	dhl: "15.00",
+	fedx: "10.00",
+};
 const PaymentMethodLayout = () => {
 	return (
 		<>
@@ -15,14 +21,14 @@ const PaymentMethodLayout = () => {
 				<h6>Payment Method</h6>
 			</div>
 			<div className="row mb-3 text-center">
-				<div className="row mb-3 text-center gap-1">
+				<div className="row mb-3 text-center gap-3 d-flex flex-wrap align-items-center justify-content-evenly">
 					{/* <div className="col-sm-4 outline-img border">
 						<img src={require("../assests/visa.png")} height={20} />
 					</div> */}
 					{paymentMethodLogos.map((item) => {
 						if (["maestro", "discover", "ideal"].includes(item)) {
 							return (
-								<div key={item} className="col-sm-3 outline-img border g-3">
+								<div key={item} className="col-sm-3 outline-img border g-1">
 									<img
 										src={require("../assests/" + item + ".png")}
 										height={30}
@@ -45,11 +51,17 @@ const PaymentMethodLayout = () => {
 			<div className="row mb-3 text-start">
 				<h6>Delivery Method</h6>
 			</div>
-			<div className="row mb-3 text-center  gap-1">
+			<div className="row mb-3 text-center gap-3 d-flex flex-wrap align-items-center justify-content-evenly">
 				{deliveryMethodLogos.map((item) => {
 					return (
-						<div key={item} className="col-sm-3 outline-img border g-3">
+						<div
+							key={item}
+							className="col-sm-5 outline-img border text-center g-1 d-flex flex-wrap align-items-center justify-content-evenly"
+						>
 							<img src={require("../assests/" + item + ".png")} height={25} />
+							<p className="m-0 pt-1 pb-1 d-flex flex-wrap align-items-center justify-content-evenly">
+								${deliveryMethodCost[item]}
+							</p>
 						</div>
 					);
 				})}
--- src/layout/PaymentPageLayout.js
+++ src/layout/PaymentPageLayout.js
@@ -3,7 +3,7 @@ import { LongButton } from "../components/ButtonComponents";
 
 const PaymentPageLayout = ({ children }) => {
 	return (
-		<div className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-center py-3 border-bottom">
+		<div className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-center mx-3 border-bottom">
 			<div className="justify-content-center justify-content-md-center my-3">
 				<div className="row mb-3 text-center">
 					<div className="col-sm-6 text-start">
@@ -15,7 +15,9 @@ const PaymentPageLayout = ({ children }) => {
 					<div className="col-lg-5 text-center">
 						{children.shippingInformation}
 					</div>
-					<div className="col-lg-4 text-center ">{children.paymentMethod}</div>
+					<div className="col-lg-4 text-center d-flex flex-wrap align-items-center justify-content-between">
+						{children.paymentMethod}
+					</div>
 					<div className="col-lg-3 ">{children.yourCart}</div>
 				</div>
 				<div className="row mb-3 text-start">
