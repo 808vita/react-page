8c9fe653ed19c051e50e7f0d90c269334aa19de8
Sudharsun <psu.fabulo@gmail.com>
2022-08-04 19:10:49 +0530

    starter

---
+++ src/layout/PaymentPageLayout.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PaymentPageLayout = () => {
+	return (
+		<>
+			<div className="row mb-3 text-center">
+				<div className="col-sm-6 themed-grid-col">.col-sm-4</div>
+				<div className="col-sm-6 themed-grid-col">.col-sm-4</div>
+			</div>
+			<div className="row mb-3 text-center">
+				<div className="col-lg-4 themed-grid-col">.col-lg-4</div>
+				<div className="col-lg-4 themed-grid-col">.col-lg-4</div>
+				<div className="col-lg-4 themed-grid-col">.col-lg-4</div>
+			</div>
+			<div className="row mb-3 text-center">
+				<div className="col-sm-6 themed-grid-col">.col-sm-4</div>
+				<div className="col-sm-6 themed-grid-col">.col-sm-4</div>
+			</div>
+		</>
+	);
+};
+
+export default PaymentPageLayout;
---
+++ src/page/PaymentPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import PaymentPageLayout from "../layout/PaymentPageLayout";
+
+const PaymentPage = () => {
+	return (
+		<>
+			<Header />
+			<PaymentPageLayout />
+		</>
+	);
+};
+
+export default PaymentPage;
